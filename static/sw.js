5ae016a24ed4f946577f5f423805a005554a656d
Ytserilo <acemirskija@gmail.com>
2021-04-27 15:24:45 +0300

    Godeps

--- static/sw.js
+++ static/sw.js
@@ -3,8 +3,12 @@ self.addEventListener('install', function(event) {
 
   event.waitUntil(
     fetch(indexPage).then(function(response) {
-      return caches.open('pwabuilder-offline').then(function(cache) {
-
+      return caches.open('pwabuilder-offline').then(async function(cache) {
+        await cache.addAll([
+          "/static/style.css",
+          "/static/create_order.js",
+          "/static/question_form.js"
+        ])
         return cache.put(indexPage, response);
       });
   }));
@@ -22,7 +26,7 @@ self.addEventListener('fetch', function(event) {
   event.waitUntil(updateCache(event.request));
   event.respondWith(
     fetch(event.request).catch(function(error) {
-      
+
       return caches.open('pwabuilder-offline').then(function (cache) {
         return cache.match(event.request).then(function (matching) {
           var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
